2e1b37e7ae926621422947b8523475fc2dfdeb86
Shailesh Pandey <shailesh.dev296@gmail.com>
2025-07-17 17:11:38 +0530

    initial-setup

---
+++ e-commerce/src/app/(pages)/listing/components/product-card/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./styles.module.css"
+function ProductCard({product}) {
+  const productName = product?.product_details?.product_name;
+  return (
+    <div>
+        <div className={`${styles.cardContainer} , ${styles.mr10}`}>
+              {productName}
+        </div>
+    </div>
+  )
+}
+
+export default ProductCard
---
+++ e-commerce/src/app/(pages)/listing/page.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import ProductCard from "./components/product-card";
+import styles from "./page.module.css"
+function ListingPage() {
+
+  const productDetails = [
+    {
+      id : 1,
+      product_details : {
+        product_name : "T-shirt",
+      }
+    } , 
+      {
+      id : 2,
+   product_details : {
+        product_name : "Jeans",
+      }
+    } , 
+      {
+      id : 3,
+   product_details : {
+        product_name : "Full-Shirt",
+      }
+    } , 
+      {
+      id : 4,
+   product_details : {
+        product_name : "Half-Shirt",
+      }
+    } , 
+      {
+      id : 5,
+   product_details : {
+        product_name : "Shorts",
+      }
+    }
+  ]
+  return (
+    <div className={styles.flex}>
+        {
+          productDetails?.map(product => <>
+            <ProductCard product={product}/>
+          </>)
+        }
+    </div>
+  )
+}
+
+export default ListingPage
